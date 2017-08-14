776708ff133be34b35d951dc9a19ad1b1ade15e0
NoEffort <noeffortofficial@gmail.com>
2017-08-14 09:48:50 -0400

    Add files via upload

---
+++ Basic/src/main/java/com/noeffort/basic/gen/BasicOreGen.java
@@ -0,0 +1,55 @@
+package com.noeffort.basic.gen;
+
+import com.noeffort.basic.init.BlockInit;
+import net.minecraft.block.state.pattern.BlockMatcher;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.IChunkGenerator;
+import net.minecraft.world.gen.feature.WorldGenMinable;
+import net.minecraft.world.gen.feature.WorldGenerator;
+import net.minecraftforge.fml.common.IWorldGenerator;
+
+import java.util.Random;
+
+public class BasicOreGen implements IWorldGenerator {
+
+    private WorldGenerator strange_ore, nether, end;
+
+    public BasicOreGen() {
+        strange_ore = new WorldGenMinable(BlockInit.strange_ore.getDefaultState(), 5);
+        nether = new WorldGenMinable(BlockInit.nether_strange_ore.getDefaultState(), 5, BlockMatcher.forBlock(Blocks.NETHERRACK));
+        end = new WorldGenMinable(BlockInit.end_strange_ore.getDefaultState(), 5, BlockMatcher.forBlock(Blocks.END_STONE));
+    }
+
+    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
+        switch(world.provider.getDimension()) {
+            case 0:
+                runGenerator(strange_ore, world, random, chunkX, chunkZ, 35, 15, 50);
+                break;
+
+            case 1:
+                runGenerator(end, world, random, chunkX, chunkZ, 25, 15, 70);
+                break;
+
+            case -1:
+                runGenerator(nether, world, random, chunkX, chunkZ, 45, 0, 255);
+                break;
+        }
+    }
+
+    private void runGenerator(WorldGenerator gen, World world, Random random, int chunkX, int chunkZ, int chance, int minHeight, int maxHeight) {
+        if (minHeight > maxHeight || minHeight < 0 || maxHeight > 256) throw new IllegalArgumentException("Ore Generated [Out of Bounds]");
+        int heightDifference = maxHeight - minHeight + 1;
+
+        for(int i = 0; i < chance; i++) {
+            int x = chunkX * 16 + random.nextInt(16);
+            int y = minHeight + random.nextInt(heightDifference);
+            int z = chunkZ * 16 + random.nextInt(16);
+
+            gen.generate(world, random, new BlockPos(x, y, z));
+        }
+    }
+
+}
