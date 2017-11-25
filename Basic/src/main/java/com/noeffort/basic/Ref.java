cf1daf8897bbbabdac9f3217d2432302942b8e0c
NoEffort <noeffortofficial@gmail.com>
2017-11-24 20:22:21 -0500

    Create Ref.java

---
+++ Basic/src/main/java/com/noeffort/basic/Ref.java
@@ -0,0 +1,12 @@
+package com.noeffort.basic;
+
+public class Ref {
+
+    public static final String MODID = "basic";
+    public static final String NAME = "Basic";
+    public static final String VERSION = "0.5 - MC 1.12";
+
+    public static final String CLIENTPROXY = "com.noeffort.basic.proxy.ClientProxy";
+    public static final String COMMONPROXY = "com.noeffort.basic.proxy.CommonProxy";
+
+}
